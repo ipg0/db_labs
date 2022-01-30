874042642739edc9af9bd1f5e2e8a4e89f838708
notblank00 <pyromancy00@gmail.com>
2022-01-30 12:03:35 +0300

    lab1 done too

--- lab1/timezone_shift_of_each_flight.sql
+++ lab1/timezone_shift_of_each_flight.sql
@@ -1,7 +1,6 @@
 select flights.flight_no,
   a_from.airport_name as departure_airport,
   a_to.airport_name as arrival_airport,
-  flights.scheduled_departure,
   tz_from.name as departure_timezone,
   tz_to.name as arrival_timezone,
   to_char(tz_from.utc_offset - tz_to.utc_offset, 'HH24:MI') as timezone_shift
@@ -10,4 +9,11 @@ from flights
   join airports a_to on flights.arrival_airport = a_to.airport_code
   join pg_timezone_names tz_from on a_from.timezone = tz_from.name
   join pg_timezone_names tz_to on a_to.timezone = tz_to.name
-order by tz_from.utc_offset - tz_to.utc_offset desc;
+group by flights.flight_no,
+  a_from.airport_name,
+  a_to.airport_name,
+  tz_from.utc_offset,
+  tz_to.utc_offset,
+  tz_from.name,
+  tz_to.name
+order by abs(extract(hours from tz_from.utc_offset - tz_to.utc_offset)) desc;
